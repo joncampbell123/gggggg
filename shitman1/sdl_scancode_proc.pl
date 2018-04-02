dfd7dc51440794bbc9dd39658860a10fab13bac6
Jonathan Campbell <jonathan@castus.tv>
2018-04-01 20:06:54 -0700

    more

---
+++ shitman1/sdl_scancode_proc.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+open(X,"cat /x | sed -e 's/[ \t]//g' | cut -d = -f 1 |") || die;
+while (my $line = <X>) {
+    chomp $line;
+    next unless $line ne "";
+
+    $new = $line;
+    $new =~ s/^SDL_SCANCODE/Game_KC/;
+
+    print substr("#define $new                                            ",0,32)."$line\n";
+}
+close(X);
+
