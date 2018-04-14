0fbd33f18304816d5436bcdd2e5714759e12d4f9
Jonathan Campbell <jonathan@castus.tv>
2018-04-14 08:44:07 -0700

    more

--- shitman1/game.h
+++ shitman1/game.h
@@ -11,10 +11,19 @@
 # include <SDL2/SDL.h>
 #endif
 
-typedef struct game_rect_t {
+typedef struct game_rectwh_t {
     int16_t             x;
     int16_t             y;
     int16_t             w;
     int16_t             h;
-} game_rect_t;
+} game_rectwh_t;
+
+/* rect is defined to cover x1 <= x < x2
+ *                      and y1 <= y < y2 */
+typedef struct game_rectxy_t {
+    int16_t             x1;
+    int16_t             y1;
+    int16_t             x2;
+    int16_t             y2;
+} game_rectxy_t;
 
