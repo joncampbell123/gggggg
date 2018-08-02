ebfe16dd625c0af59a03539764df8b50e36b37a1
Jonathan Campbell <jonathan@castus.tv>
2018-08-02 16:36:25 -0700

    more

--- renameui/renameui.cpp
+++ renameui/renameui.cpp
@@ -2,9 +2,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+
+#include <string>
+#include <vector>
+
+std::vector< std::pair<std::string,struct stat> >       dirlist;
+int                                                     dirlist_sel = -1;
 
 int main() {
     return 0;
