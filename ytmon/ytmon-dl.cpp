a8699683544b4b524efd515e200d88bc28f55502
root <root@nas.jmc>
2020-01-11 17:26:38 -0800

    up a bit

--- ytmon/ytmon-dl.cpp
+++ ytmon/ytmon-dl.cpp
@@ -88,7 +88,7 @@ bool download_video_youtube(const Json &video) {
         creds += string("--username '") + youtube_user + "' --password '" + youtube_pass + "' ";
 
     {
-        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --all-subs --limit-rate=1000K --output '%(id)s' ") + creds + invoke_url;
+        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --all-subs --limit-rate=1500K --output '%(id)s' ") + creds + invoke_url;
         int status = system(cmd.c_str());
         if (WIFSIGNALED(status)) should_stop = true;
         if (status != 0) return false;
