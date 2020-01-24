6360a59b7f295e54cb81c0027e50ad209f114dfe
Jonathan Campbell <jonathan@castus.tv>
2020-01-23 23:21:37 -0800

    OK, IRQ done

--- techno/game.c
+++ techno/game.c
@@ -26,14 +26,134 @@
 #include <hw/vga/vgatty.h>
 #include <hw/dos/doswin.h>
 
-unsigned int            TIMER_TICK_RATE = 300;
+struct timer_event_t {
+    uint32_t                            time;
+    uint32_t                            user;
+    void                                (*callback)(uint32_t user);
+    volatile struct timer_event_t*      next;
+};
 
-volatile uint32_t       tick_count = 0;
-void                    (__interrupt __far *old_tick_irq)() = NULL;
+unsigned int                    TIMER_TICK_RATE = 300;
+
+volatile uint32_t               tick_count = 0;
+volatile uint32_t               tick_irq_count = 0;
+void                            (__interrupt __far *old_tick_irq)() = NULL;
+
+volatile struct timer_event_t*  timer_next_irq = NULL;
 
 void __interrupt __far tick_timer_irq() {
+    while (timer_next_irq != NULL && tick_irq_count >= timer_next_irq->time) {
+        volatile struct timer_event_t *current = timer_next_irq;
+        volatile struct timer_event_t *next = current->next;
+
+        timer_next_irq = next;
+        current->next = NULL;
+        current->callback(current->user);
+    }
+
+    if (timer_next_irq == NULL)
+        tick_irq_count = 0;
+
+    tick_irq_count++;
     tick_count++;
+
     p8259_OCW2(0,P8259_OCW2_NON_SPECIFIC_EOI);
+}
+
+void schedule_timer_irq_event(volatile struct timer_event_t *ev,uint32_t time) {
+    ev->time = time + tick_irq_count;
+
+    SAVE_CPUFLAGS( _cli() ) {
+        if (timer_next_irq == NULL) {
+            /* list is empty */
+            ev->next = NULL;
+            timer_next_irq = ev;
+        }
+        else if (ev->time < timer_next_irq->time) {
+            /* new event is newer than top of list */
+            ev->next = timer_next_irq;
+            timer_next_irq = ev;
+        }
+        else {
+            volatile struct timer_event_t *s = timer_next_irq;
+
+            while (s != NULL) {
+                if (s->next != NULL) {
+                    /* if the next entry happens later, insert here */
+                    if (ev->time < s->next->time) {
+                        ev->next = s->next; /* old s->next */
+                        s->next = ev; /* new s->next */
+                        break;
+                    }
+                    /* keep looking */
+                    else {
+                        s = s->next;
+                    }
+                }
+                else {
+                    /* stick it on the end */
+                    ev->next = NULL; /* s->next == NULL */
+                    s->next = ev;
+                    break;
+                }
+            }
+        }
+    } RESTORE_CPUFLAGS();
+}
+
+void beeper_cb(uint32_t t);
+void blah2_cb(uint32_t t);
+void blah_cb(uint32_t t);
+
+struct timer_event_t blah = {
+    0,//delay
+    0,//user
+    blah_cb,//callback
+    NULL//next
+};
+
+struct timer_event_t blah2 = {
+    0,//delay
+    0,//user
+    blah2_cb,//callback
+    NULL//next
+};
+
+struct timer_event_t beeper = {
+    0,//delay
+    0,//user
+    beeper_cb,//callback
+    NULL//next
+};
+
+void blah_cb(uint32_t t) {
+    (void)t;
+
+    ( *((uint16_t far*)MK_FP(0xB800,0x0000)) )++;
+
+    schedule_timer_irq_event(&blah,100);
+}
+
+void blah2_cb(uint32_t t) {
+    (void)t;
+
+    ( *((uint16_t far*)MK_FP(0xB800,0x0002)) )++;
+
+    schedule_timer_irq_event(&blah2,3);
+}
+
+void beeper_cb(uint32_t t) {
+    (void)t;
+
+    __asm {
+        push    ax
+        in      al,61h
+        xor     al,3h
+        out     61h,al
+        pop     ax
+    }
+
+    schedule_timer_irq_event(&beeper,300);
 }
 
 int main(int argc,char **argv,char **envp) {
@@ -66,25 +186,42 @@ int main(int argc,char **argv,char **envp) {
     _dos_setvect(irq2int(0),tick_timer_irq);
     p8259_unmask(T8254_IRQ);
 
+    schedule_timer_irq_event(&beeper,300);
+    schedule_timer_irq_event(&blah,100);
+    schedule_timer_irq_event(&blah2,3);
+
     for (;;) {
         if (kbhit()) {
             int c = getch();
             if (c == 27) break;
+
+            if (c == ' ') {
+                p8259_mask(T8254_IRQ);
+                {
+                    volatile struct timer_event_t *t = timer_next_irq;
+                    fprintf(stderr,"\n");
+                    while (t != NULL) {
+                        fprintf(stderr,"t=%lu p=%p\n",(unsigned long)t->time,(void*)t);
+                        t = t->next;
+                    }
+                    fprintf(stderr,"\n");
+                }
+                p8259_unmask(T8254_IRQ);
+            }
         }
 
-#if 1
         {
-            uint32_t t;
+            uint32_t t,ti;
             SAVE_CPUFLAGS( _cli() ) {
                 t = tick_count;
+                ti = tick_irq_count;
             } RESTORE_CPUFLAGS();
             if (ptick_count != t) {
                 ptick_count = t;
-                printf("\x0D" "%lu  ",(unsigned long)ptick_count);
+                printf("\x0D" "%lu/IRQ=%lu  ",(unsigned long)t,(unsigned long)ti);
                 fflush(stdout);
             }
         }
-#endif
     }
 
     /* timer unsetup */
