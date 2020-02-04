3cf31cf0044f693be39ef4cc8916441ca265c2f5
Jonathan Campbell <jonathan@castus.tv>
2020-02-03 23:29:37 -0800

    more

--- techno/game.c
+++ techno/game.c
@@ -133,10 +133,8 @@ static void video_hline_inner_span2m(const unsigned int x1,const unsigned int x2
     video_wrvmaskv(vp++,cga4leftmask(x1),(unsigned char)wbm);
     if ((--xc) != 0u) { /* leftmost edge counts. there may be middle to fill. */
         /* middle part that completely covers the byte */
-        if (xc & (~1u))
-            vp = video_wr16mset(vp,wbm,xc>>1u);
-        if (xc & 1u)
-            video_wr(vp++,(unsigned char)wbm);
+        vp = video_wr16mset(vp,wbm,xc>>1u); /* REP MOVSW with CX == 0 does nothing, safe */
+        if (xc & 1u) video_wr(vp++,(unsigned char)wbm);
     }
     /* rightmost edge that may or may not cover the entire byte */
     video_wrvmaskv(vp,cga4rightmask(x2),(unsigned char)wbm);
