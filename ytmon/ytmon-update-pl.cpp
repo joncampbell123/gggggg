5e559a7058bf68435b4a0a66b5cc35ddecebd129
Jonathan Campbell <jonathan@castus.tv>
2020-02-06 22:21:36 -0800

    playlist load/save/combine

--- ytmon/ytmon-update-pl.cpp
+++ ytmon/ytmon-update-pl.cpp
@@ -10,9 +10,105 @@
 #include <json11.hpp>
 
 #include <string>
+#include <algorithm>
 
 using namespace std;
 using namespace json11;
+
+struct js_entry {
+    string      json;
+
+    bool operator==(const struct js_entry &e) const {
+        return  json == e.json;
+    }
+};
+
+char line_tmp[4096];
+
+void chomp(char *s) {
+	char *e = s + strlen(s) - 1;
+	while (e >= s && (*e == '\n' || *e == '\r')) *e-- = 0;
+}
+
+int load_js_list(vector<js_entry> &js,const string &jsfile) {
+    js.clear();
+
+    FILE *fp = fopen(jsfile.c_str(),"r");
+    if (fp == NULL) return -1; // sets errno
+
+    js_entry cur;
+    bool cur_set = false;
+
+    while (!feof(fp) && !ferror(fp)) {
+        if (fgets(line_tmp,sizeof(line_tmp),fp) == NULL) break;
+        if (line_tmp[0] == 0) continue;
+        if (line_tmp[0] == '#') continue;
+        chomp(line_tmp);
+
+        // JSON
+        cur.json = line_tmp;
+        cur_set = true;
+
+        // emit
+        if (cur_set) {
+            js.push_back(cur);
+            cur_set = false;
+            cur = js_entry();
+        }
+    }
+
+    // emit
+    if (cur_set) {
+        js.push_back(cur);
+        cur_set = false;
+        cur = js_entry();
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int write_js_list(const string &jsfile,vector<js_entry> &js) {
+    FILE *fp = fopen(jsfile.c_str(),"w");
+    if (fp == NULL) return -1; // sets errno
+
+    for (auto jsi=js.begin();jsi!=js.end();jsi++) {
+        auto &jsent = *jsi;
+        if (jsent.json.empty()) continue;
+        fprintf(fp,"%s\n",jsent.json.c_str());
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int combine_js(const string &dst,const string &js1,const string &js2) {
+    vector<js_entry> j1,j2;
+
+    if (load_js_list(j1,js1) < 0) {
+        if (errno != ENOENT) { // it's OK if it doesn't exist
+            fprintf(stderr,"Cannot load %s\n",js1.c_str());
+            return -1;
+        }
+    }
+
+    if (load_js_list(j2,js2) < 0) {
+        fprintf(stderr,"Cannot load %s\n",js2.c_str());
+        return -1;
+    }
+
+    for (auto j2i=j2.begin();j2i!=j2.end();j2i++) {
+        if (find(j1.begin(),j1.end(),*j2i) == j1.end())
+            j1.push_back(*j2i);
+    }
+
+    if (write_js_list(dst,j1) < 0) {
+        fprintf(stderr,"Cannot write %s\n",dst.c_str());
+        return -1;
+    }
+
+    return 0;
+}
 
 int main(int argc,char **argv) {
     string api_url;
@@ -37,6 +133,7 @@ int main(int argc,char **argv) {
             tm.tm_sec - (tm.tm_sec % 60)); // TODO: reduce to 15 seconds if confident
     }
     string js_tmp_file = string("playlist-tmp-") + timestr + ".js";
+    string js_mix_tmp = "playlist-tmp-combine.js";
 
     {
         struct stat st;
@@ -67,8 +164,11 @@ int main(int argc,char **argv) {
         }
     }
 
-    /* TODO: Combine tmp with js only if download completed. */
-
+    if (combine_js(js_mix_tmp,js_file,js_tmp_file)) {
+        fprintf(stderr,"Failed to combine JS\n");
+        return 1;
+    }
+    rename(js_mix_tmp.c_str(),js_file.c_str());
     return 0;
 }
 
