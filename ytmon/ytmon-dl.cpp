d0994e6a902ad610fca706aa19ba8d0b281e89fc
root <root@nas.jmc>
2020-03-02 07:52:29 -0800

    remove embed-thumbnails

--- ytmon/ytmon-dl.cpp
+++ ytmon/ytmon-dl.cpp
@@ -120,7 +120,7 @@ bool download_video_youtube(const Json &video) {
     time_t dl_begin = time(NULL);
 
     {
-        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --add-metadata --write-all-thumbnails --embed-thumbnail --write-info-json --embed-subs --all-subs --limit-rate=") + to_string(youtube_bitrate) + "K --output '%(id)s' " + creds + invoke_url;
+        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --add-metadata --write-all-thumbnails --write-info-json --embed-subs --all-subs --limit-rate=") + to_string(youtube_bitrate) + "K --output '%(id)s' " + creds + invoke_url;
         int status = system(cmd.c_str());
         if (WIFSIGNALED(status)) should_stop = true;
 
@@ -209,7 +209,7 @@ bool download_video_bitchute(const Json &video) {
 
     /* All video on BitChute is .mp4, and youtube-dl needs to be given that suffix */
     {
-        string cmd = string("youtube-dl --no-check-certificate --no-mtime --continue --add-metadata --write-all-thumbnails --embed-thumbnail --write-info-json --embed-subs --all-subs --limit-rate=2000K --output '%(id)s.mp4' ") + invoke_url;
+        string cmd = string("youtube-dl --no-check-certificate --no-mtime --continue --add-metadata --write-all-thumbnails --write-info-json --embed-subs --all-subs --limit-rate=2000K --output '%(id)s.mp4' ") + invoke_url;
         int status = system(cmd.c_str());
         if (WIFSIGNALED(status)) should_stop = true;
 
