1ffff6d57020e9b681140f1e90fb3d93df8eb469
Jonathan Campbell <jonathan@castus.tv>
2020-03-20 16:36:35 -0700

    stop trying to embed subtitles, no need

--- ytmon/ytmon-dl.cpp
+++ ytmon/ytmon-dl.cpp
@@ -193,7 +193,7 @@ bool download_video_youtube(const Json &video) {
 
     /* then download the video */
     {
-        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --add-metadata --write-all-thumbnails --embed-subs --all-subs --limit-rate=") + to_string(youtube_bitrate) + "K --output '%(id)s' " + creds + invoke_url; /* --write-info-json not needed, first step above */
+        string cmd = string("youtube-dl --cookies cookies.txt --no-mtime --continue --add-metadata --write-all-thumbnails --all-subs --limit-rate=") + to_string(youtube_bitrate) + "K --output '%(id)s' " + creds + invoke_url; /* --write-info-json not needed, first step above */
         int status = system(cmd.c_str());
         if (WIFSIGNALED(status)) should_stop = true;
 
@@ -282,7 +282,7 @@ bool download_video_bitchute(const Json &video) {
 
     /* All video on BitChute is .mp4, and youtube-dl needs to be given that suffix */
     {
-        string cmd = string("youtube-dl --no-check-certificate --no-mtime --continue --add-metadata --write-all-thumbnails --write-info-json --embed-subs --all-subs --limit-rate=") + to_string(bitchute_bitrate) + "K --output '%(id)s.mp4' " + invoke_url;
+        string cmd = string("youtube-dl --no-check-certificate --no-mtime --continue --add-metadata --write-all-thumbnails --write-info-json --all-subs --limit-rate=") + to_string(bitchute_bitrate) + "K --output '%(id)s.mp4' " + invoke_url;
         int status = system(cmd.c_str());
         if (WIFSIGNALED(status)) should_stop = true;
 
